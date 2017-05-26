1f6cebc222e07f479929bb6514b0c9f8d3e5bf4f
Andrew Lamarra <andrew.lamarra@gmail.com>
2017-05-25 22:08:52 -0500

    Adding a validator

--- pynalyze.py
+++ pynalyze.py
@@ -2,8 +2,11 @@
 """ pynalyze is a URL analyzer
 AUTHOR: Andrew Lamarra
 """
+import validators
+
 
 # Prompt the user for the URL
 url = input("Enter a URL to analyze: ")
 
 # Validate that it IS a URL... This will be difficult
+print(validators.url(url))
