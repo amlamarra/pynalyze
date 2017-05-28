62ffc1451f50513e62f2381ff8b5e9684566617c
Andrew Lamarra <andrew.lamarra@gmail.com>
2017-05-28 11:39:29 -0500

    Can now pass the URL as an argument

--- pynalyze.py
+++ pynalyze.py
@@ -3,10 +3,30 @@
 AUTHOR: Andrew Lamarra
 """
 import validators
+import argparse
 
 
-# Prompt the user for the URL
-url = input("Enter a URL to analyze: ")
+if __name__ == "__main__":
 
-# Validate that it IS a URL... This will be difficult
-print(validators.url(url))
+    parser = argparse.ArgumentParser(description="Checks if a URL is malicious")
+    parser.add_argument("URL", nargs="?", help="Provide the URL")
+    args = parser.parse_args()
+
+    # Prompt the user for the URL if none were supplied
+    if args.URL:
+        url = args.URL
+    else:
+        url = input("Enter a URL to analyze: ")
+
+    if "://" not in url:
+        url = "http://" + url
+    protocol = url.split("://")[0]
+    if protocol != "http" and protocol != "https":
+        print("This only accepts either the HTTP or HTTPS protocol")
+
+    # Validate that it IS a URL
+    print("\nValidating {}".format(url))
+    if validators.url(url):
+        print("Valid URL")
+    else:
+        print("Invalid URL")
